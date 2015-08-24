90a1021d2c128e40eed6ca9a2c55eeadb2c6247e
Bjoern Griebenow <b.griebenow@web.de>
2015-08-24 16:03:39 +0200

    Integrated robe (Ruby IDE features) into config.

---
+++ config/language_ruby.el
@@ -0,0 +1,10 @@
+(require 'robe)
+
+(inf-ruby)
+(robe-start)
+
+(eval-after-load 'company
+  '(push 'company-robe company-backends))
+
+(defadvice inf-ruby-console-auto (before activate-rvm-for-robe activate)
+    (rvm-activate-corresponding-ruby))
