f0b0d9d3452c0dfb5ea233425228188f8ffb0bd1
Bjoern Griebenow <b.griebenow@web.de>
2016-03-24 06:31:26 +0100

    Added  C/C++ header auto completion usig irony and company

--- config/language_c.el
+++ config/language_c.el
@@ -1,3 +1,14 @@
 (require 'company)
 
 (add-to-list 'company-backends 'company-c_headers)
+
+;; Load with `irony-mode` as a grouped backend
+(require 'company-irony-c-headers)
+
+(eval-after-load 'company
+'(add-to-list
+'company-backends '(company-irony-c-headers company-irony)))
+
+;; When compiler options change, call
+;; `company-irony-c-headers-reload-compiler-output` manually to
+;; reload.
