9dc5dc399fb4d5fbd635206bbde31583942c7824
Bjoern Griebenow <b.griebenow@web.de>
2016-11-28 15:50:19 +0100

    Disable schrute mode

--- config/help.el
+++ config/help.el
@@ -5,8 +5,8 @@
 ;;; Code:
 
 
-;; Print alternative shorter ways of doing things.
-(setf schrute-shortcuts-commands '((avy-goto-line   . (next-line previous-line))
-                                   (avy-goto-word-1 . (left-char right-char))))
+;; ;; Print alternative shorter ways of doing things.
+;; (setf schrute-shortcuts-commands '((avy-goto-line   . (next-line previous-line))
+;;                                    (avy-goto-word-1 . (left-char right-char))))
 
-(schrute-mode) ;; and turn on the minor mode.
+;; (schrute-mode) ;; and turn on the minor mode.
