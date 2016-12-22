963daefecfdb59e4cee4c8a3b7fd61b35a9ec0fe
Bjoern Griebenow <b.griebenow@web.de>
2016-12-22 15:09:07 +0100

    Add autogenerated theme section in init.el

--- init.el
+++ init.el
@@ -12,3 +12,20 @@
 (package-initialize)
 
 (load "~/.emacs.d/my_init.el")
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   (quote
+    ("1c50040ec3b3480b1fec3a0e912cac1eb011c27dd16d087d61e72054685de345" default)))
+ '(package-selected-packages
+   (quote
+    (jade js-comint js2-mode bln-mode importmagic indent-tools ivy-rich zerodark-theme zenburn-theme yasnippet web-mode-edit-element srefactor spacemacs-theme smex seti-theme schrute ruby-refactor robe rainbow-delimiters neotree magit-svn magit-gitflow magit-find-file irony-eldoc hungry-delete hipster-theme hc-zenburn-theme flycheck-irony flycheck-bashate flx-ido f3 emr darkokai-theme darkane-theme cyberpunk-theme coverlay company-irony-c-headers company-irony badger-theme avy-flycheck atom-one-dark-theme atom-dark-theme ample-theme))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(whitespace-tab ((t (:background "red")))))
