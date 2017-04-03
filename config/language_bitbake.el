00ddb4d8509d6b0847b475106acdbb6f3a1fffc8
Bjoern Griebenow <b.griebenow@web.de>
2017-04-03 10:13:11 +0200

    Fix syntax error in bitbake config file

--- config/language_bitbake.el
+++ config/language_bitbake.el
@@ -6,6 +6,5 @@
 
 (require 'bitbake)
 
-sddsf
-(add-tsdfo-list 'auto-mode-alist '("\\.bb\\'"       . bitbake-mode))
+(add-to-list 'auto-mode-alist '("\\.bb\\'"       . bitbake-mode))
 (add-to-list 'auto-mode-alist '("\\.bbappend\\'" . bitbake-mode))
