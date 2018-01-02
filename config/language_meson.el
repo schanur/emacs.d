827e785e6a9085eab3a27a3df9269dff32392c0f
Bjoern Griebenow <b.griebenow@web.de>
2018-01-02 01:58:45 +0100

    Use meson mode with company

---
+++ config/language_meson.el
@@ -0,0 +1,11 @@
+;;; package --- Summary
+
+;;; Commentary:
+
+;;; Code:
+
+(add-hook 'meson-mode-hook 'company-mode)(add-hook 'meson-mode-hook 'company-mode)
+
+(provide 'language_meson)
+
+;;; language_meson ends here
