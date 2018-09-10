4fc4324fe3e9aaeb61d6a2960622274d71acf731
Bjoern Griebenow <b.griebenow@web.de>
2018-09-11 00:40:42 +0200

    Use irony in C/C++/ObjC

--- config/language_c.el
+++ config/language_c.el
@@ -11,12 +11,20 @@
 ;; Load with `irony-mode` as a grouped backend
 ;; (require 'company-irony-c-headers)
 
+(add-hook 'c++-mode-hook  'irony-mode)
+(add-hook 'c-mode-hook    'irony-mode)
+(add-hook 'objc-mode-hook 'irony-mode)
+
 (eval-after-load 'company
 '(add-to-list
 'company-backends '(company-irony-c-headers company-irony)))
 
 (eval-after-load 'flycheck
     '(add-hook 'flycheck-mode-hook #'flycheck-irony-setup))
+
+;; Use compile_commands.json database.
+(add-hook 'irony-mode-hook 'irony-cdb-autosetup-compile-options)
+;; (add-hook 'irony-mode-hook 'company-irony-setup-begin-commands)
 
 ;; When compiler options change, call
 ;; `company-irony-c-headers-reload-compiler-output` manually to
