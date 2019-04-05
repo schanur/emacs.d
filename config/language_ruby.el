d3526d16f98570331621f7bbcae6aca6703aebde
Bjoern Griebenow <b.griebenow@web.de>
2019-04-05 11:47:30 +0200

    Start Ruby modes when first Ruby file gets opened

--- config/language_ruby.el
+++ config/language_ruby.el
@@ -6,11 +6,18 @@
 
 (require 'robe)
 
-(inf-ruby)
-(robe-start)
+;; (inf-ruby)
+;; (robe-start)
+
+
+
+(add-hook 'ruby-mode-hook 'robe-mode)
+
+(autoload 'inf-ruby-minor-mode "inf-ruby" "Run an inferior Ruby process" t)
+(add-hook 'ruby-mode-hook 'inf-ruby-minor-mode)
 
 (eval-after-load 'company
   '(push 'company-robe company-backends))
 
 (defadvice inf-ruby-console-auto (before activate-rvm-for-robe activate)
-    (rvm-activate-corresponding-ruby))
+  (rvm-activate-corresponding-ruby))
