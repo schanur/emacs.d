2b2564c82b7fb379e794304fbde7e626dd15e3c3
ze <ze@mizar>
2022-05-03 22:29:14 +0200

    feat: use global projectile key mapping

--- config/projectile.el
+++ config/projectile.el
@@ -1,3 +1,9 @@
 (use-package projectile
-    :ensure t
+  :ensure t
+  :custom (projectile-indexing-method 'alien)
+  :init
+  (projectile-mode +1)
+  :bind (:map projectile-mode-map
+              ("s-p" . projectile-command-map)
+              ("C-c p" . projectile-command-map))
 )
