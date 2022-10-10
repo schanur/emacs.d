d53154c8da9f2c8965de9826bd87008bca746ea8
Bjoern Griebenow <b.griebenow@web.de>
2022-10-10 15:50:41 +0200

    refactor: add missing emacs config files

---
+++ customize_ui.el
@@ -0,0 +1,15 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   '("6b6bad9d7a844d5de02761a1bd155869512f47bd6a7b14d799eea5c37f08ead4" "43f03c7bf52ec64cdf9f2c5956852be18c69b41c38ab5525d0bedfbd73619b6a" "816bacf37139d6204b761fea0d25f7f2f43b94affa14aa4598bce46157c160c2" "fc5fcb6f1f1c1bc01305694c59a1a861b008c534cae8d0e48e4d5e81ad718bc6" default))
+ '(package-selected-packages
+   '(keycast fzf dashboard c-eldoc undo-tree zzz-to-char zerodark-theme zenburn-theme yasnippet-snippets xah-lookup which-key web-mode-edit-element vscdark-theme visual-regexp use-package treemacs-projectile systemd sunburn-theme ssh-deploy srefactor spacemacs-theme soothe-theme smex seti-theme schrute rustic rust-playground ruby-refactor robe realgud-lldb rainbow-delimiters racer projectile-ripgrep powerthesaurus php-refactor-mode paradox origami opencl-mode nyx-theme nv-delete-back neotree naysayer-theme mood-one-theme monokai-theme monokai-pro-theme moe-theme meson-mode material-theme magit-svn magit-gitflow magit-find-file madhat2r-theme lsp-ui lsp-treemacs kaolin-themes js2-refactor js2-highlight-vars js2-closure js-comint irony-eldoc immaterial-theme hungry-delete humanoid-themes horizon-theme highlight-parentheses helm-tramp helm-themes helm-rtags helm-rg helm-projectile helm-lsp helm-gtags helm-fuzzy-find helm-fuzzier helm-flyspell helm-flycheck helm-flx helm-dictionary helm-dash helm-cscope helm-c-yasnippet helm-ag hc-zenburn-theme green-is-the-new-black-theme gitignore-mode gitconfig-mode gitattributes-mode flycheck-rust flycheck-irony flx-ido exotica-theme esup emr elpy eglot dumb-jump dtrt-indent doom-themes diff-hl cyberpunk-theme cyberpunk-2019-theme constant-theme company-web company-statistics company-shell company-rtags company-racer company-php company-lsp company-irony-c-headers company-irony company-flx company-anaconda colorless-themes color-theme-solarized chocolate-theme challenger-deep-theme cargo brutalist-theme bitbake badger-theme atom-one-dark-theme atom-dark-theme arc-dark-theme ample-theme alect-themes ag abyss-theme)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(whitespace-tab ((t (:background "red")))))
